84fe3d7dc0d1b90cc6944e49d6667b7be8e97597
Phoenix Perry <contact@phoenixperry.com>
2012-10-08 11:01:58 -0400

    making sure everything is sorted out

--- Object/sheet.js
+++ Object/sheet.js
@@ -1,11 +1,14 @@
 var stage, outfit, container;
+var fred; 
 window.onload = function () {init();} 
 
 function init() { 
 	
-	var fred = new myCharacter("sam", 100,100);  
-	
-	 console.log(fred.name);
+	fred = new myCharacter("sam", 100,100, "roy");  
+	fred.setValue("roy");
+	console.log(fred.getValue()); 
+	//note you can get the value name...
+
 
 		stage = new createjs.Stage("demoCanvas"); 
 		circle = new createjs.Shape(); 
@@ -15,21 +18,45 @@ function init() {
 		circle.y = 0; 
 		createjs.Ticker.setFPS(60);
 		createjs.Ticker.addListener(this); 
-
+		addingAndDeletingProperties(); 
 }
 
  function tick() { 
  		stage.update();
  }
-
+//here's an object being created 
 function myCharacter(name, xpos, ypos)
 {
+	//not using this make a field public 
+	var val;
+	//make this local to the function to 
+	//effectively make it private 
 	this.name = name; 
 	this.xpos = xpos; 
 	this.ypos = ypos; 
 	this.getDressed = getDressed; 
+	this.age; //properties can be setup later 
+	//handling getters and setters 
+	this.getValue = function(){ 
+		return val; 
+	}; 
+
+	this.setValue = function (_temp){ 
+		val = _temp; 
+	};	
 }
+
+
 function getDressed(bits) { 
 
 	console.log("getting changed"); 
+}
+function addingAndDeletingProperties(){ 
+		fred.age = 32; 
+		console.log(fred.age); 
+		//here's an object having a property removed 
+		delete fred.age; 
+		//now if I print his age. i'll get 
+		//undefined. 
+		console.log(fred.age); 
 }
