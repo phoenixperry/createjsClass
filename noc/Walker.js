cef307eb3c91b889836011f048e4b311daf42d29
Phoenix Perry <contact@phoenixperry.com>
2012-11-14 12:07:41 -0500

    Nature of Code java to javascript port started

---
+++ noc/Walker.js
@@ -0,0 +1,57 @@
+
+var stage; 
+var walker; 
+window.onload = function () {init();}
+
+function init() { 
+	//this is the same as setup
+	stage = new createjs.Stage("canvas");
+	createjs.Ticker.setFPS(60); 
+	createjs.Ticker.addListener(this);
+
+	var x = stage.canvas.width/2; 
+	var y = stage.canvas.height/2;
+	walker = new Walker(x,y); 
+	walker.step(walker.x,walker.y); 
+	walker.display(walker.x,walker.y); 
+}
+function tick() { 
+	stage.update(); 
+	walker.step(walker.x,walker.y); 
+	walker.display(walker.x,walker.y); 
+}
+
+//end of the createjs setup
+
+function Walker(x,y) { 
+	this.x = x; 
+	this.y = y; 
+	this.step = step; 
+	this.display=display; 
+	this.circle = new createjs.Shape();
+}
+function display(x,y){  
+	this.circle.graphics.beginFill("black").drawCircle(x,y,2); 
+	stage.addChild(this.circle); 
+	var num = stage.getNumChildren(); 
+	console.log("only ", num, " child");
+}
+function step(x,y) { 
+	var choice = Math.round(Math.random()*4); 
+	console.log(choice); 
+	switch(choice){ 
+		case 0:
+		this.x++;  
+		break;
+		case 1:
+		this.x--;  
+		break;
+		case 2: 
+		this.y++; 
+		break;
+		case 3: 
+		this.y--; 
+		break; 
+	}
+		
+}
